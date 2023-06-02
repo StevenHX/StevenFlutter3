a7cf803020642a143d6cd42203657cefc5d8c115
huangxiao <huangxiao@wingtech.com>
2023-06-02 18:05:02 +0800

    init2

---
+++ lib/res/constant.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/foundation.dart';
+
+class Constant {
+
+  static const String theme = 'AppTheme';
+  static const String locale = 'locale';
+
+}
---
+++ lib/res/resources.dart
@@ -0,0 +1,5 @@
+
+export 'colors.dart';
+export 'dimens.dart';
+export 'gaps.dart';
+export 'styles.dart';
